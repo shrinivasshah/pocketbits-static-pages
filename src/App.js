f04f27b9d1e5aa7dcfb7e9ef8ba9f9d80be13848
shrinivasshah <shrinivasshah05@gmail.com>
2021-01-27 11:17:33 +0530

    finishe d header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import ContactUs from "./pages/ContactUs";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import FeesPage from "./pages/FeesPage";
 import Navbar from "./components/Navbar/Navbar";
+import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
         <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route path="/contact-us">
           <ContactUs />
         </Route>
         <Route path="/fees">
